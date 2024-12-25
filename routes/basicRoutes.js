d8b093ef08d5c9e65217dbb417e75fd26b3e5380
NanduNanduz <nandunsb99@gmail.com>
2024-12-25 12:16:35 +0530

    updated routes to handle put and delete operation

--- routes/basicRoutes.js
+++ routes/basicRoutes.js
@@ -62,18 +62,47 @@ function employeeRoutes(nav){
     res.redirect('/employee');
   });
 
-  //Update Operation
-  router.put('/edit/:id', (req,res)=>{
-    data.splice(req.params.id, 1, req.body);
-    res.send(data);
+  // //Update Operation
+  // router.put('/edit/:id', (req,res)=>{
+  //   data.splice(req.params.id, 1, req.body);
+  //   res.send(data);
+  // });
+
+
+
+  router.put("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = data.findIndex((employee) => employee.id === id);
+
+    if (index === -1) {
+      return res.status(404).send("Employee not found");
+    }
+
+    data[index] = { id, ...req.body }; // Update employee data
+    res.status(200).send(data);
   });
 
-  //Delete Operation
-  router.delete('/remove/:id', (req, res)=>{
-    data.pop();
-    res.send(data);
 
-  })
+  // //Delete Operation
+  // router.delete('/remove/:id', (req, res)=>{
+  //   data.pop();
+  //   res.send(data);
+
+  // })
+
+
+  router.delete("/remove/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = data.findIndex((employee) => employee.id === id);
+
+    if (index === -1) {
+      return res.status(404).send("Employee not found");
+    }
+
+    data.splice(index, 1); // Remove employee from the array
+    res.status(200).send(data);
+  });
+
 
   return router; //for to access all crud operations
 }
